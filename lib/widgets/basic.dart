edfedc51db768b3297977bf6252510b4171126b7
Pavel401 <pavelalam401@gmail.com>
2021-08-13 01:31:43 +0530

    minor chnges

--- lib/widgets/basic.dart
+++ lib/widgets/basic.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:learn_c/widgets/details.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class basic_page_ui extends StatefulWidget {
-  const basic_page_ui({ Key? key }) : super(key: key);
+  const basic_page_ui({Key? key}) : super(key: key);
 
   @override
   _basic_page_uiState createState() => _basic_page_uiState();
@@ -12,44 +13,54 @@ class basic_page_ui extends StatefulWidget {
 class _basic_page_uiState extends State<basic_page_ui> {
   @override
   Widget build(BuildContext context) {
+    final db = FirebaseFirestore.instance;
 
-    final db= FirebaseFirestore.instance;  
-  
     return Scaffold(
-
       body: StreamBuilder<QuerySnapshot>(
+        stream: db.collection('BASIC').snapshots(),
+        builder: (context, AsyncSnapshot snapshots) {
+          if (!snapshots.hasData) {
+            return Center(
+              child: Container(
+                child: Column(
+                  children: [
 
-
-      stream: db.collection('BASIC').snapshots(),
-      builder: (context,AsyncSnapshot snapshots){
-        if(!snapshots.hasData)
-        {
-          return Center(
-              child: CircularProgressIndicator(),
+                    CircularProgressIndicator(),
+                    DefaultTextStyle(
+                      style: TextStyle(fontSize: 20.0),
+                      child: AnimatedTextKit(
+                        animatedTexts: [
+                          WavyAnimatedText('Loading Data...'),
+                        ],
+                        isRepeatingAnimation: true,
+                      ),
+                    ),
+                    
+                  ],
+                ),
+              ),
             );
-        }
-        else  if (snapshots.connectionState == ConnectionState.waiting) {
-                return Text("Loading");
-                
-                }
-       else         
-       return ListView(
-
-              
-              children: snapshots.data.docs.map<Widget>((doc) {
+          } else if (snapshots.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          } else
+            return ListView(
+              children: snapshots.data!.docs.map<Widget>((doc) {
                 return Card(
                   child: ListTile(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => detail_fetch(post: doc)));
+                    },
+                    
                     title: Text(doc.data()['TITLE']),
                   ),
                 );
               }).toList(),
             );
-      },
-      
-    ),
+        },
+      ),
     );
-    
-    
-    
   }
 }
--- lib/widgets/homepage.dart
+++ lib/widgets/homepage.dart
@@ -1,94 +1,96 @@
 import 'package:flutter/material.dart';
- import 'package:learn_c/widgets/basic.dart';
+import 'package:learn_c/widgets/basic.dart';
 
 class HOMEPAGE extends StatelessWidget {
-  const HOMEPAGE({ Key? key }) : super(key: key);
+  const HOMEPAGE({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-     List<String> events=[
-
-          "BASICS",
-          "ARRAYS",
-          "STRINGS",
-          "DSA",
-          "FUNTIONS",
-          "POINTERS",
+    List<String> events = [
+      "BASICS",
+      "ARRAYS",
+      "STRINGS",
+      "DSA",
+      "FUNTIONS",
+      "POINTERS",
     ];
     return Container(
-          margin: EdgeInsets.only(top: 120),
-          child: GridView(
-            
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount:2 ),
+      margin: EdgeInsets.only(top: 120),
+      child: GridView(
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
 
-            children:events.map((title){
-              //Loop all events string 
+        children: events.map((title) {
+          //Loop all events string
 
-              return GestureDetector(
-                
-                //GestureDetector is used to get ontap feedback with card 
-                child: Card(
-                  
-                  margin: const EdgeInsets.all(20),
-                  child: getCardByTitle(title),
-                ),
+          return GestureDetector(
+            //GestureDetector is used to get ontap feedback with card
+            child: Card(
+              margin: const EdgeInsets.all(20),
+              child: getCardByTitle(title),
+            ),
 
-                onTap: (){
+            onTap: () {
+              if (title == "BASICS") {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => basic_page_ui()),
+                );
+              }
+              //Ontap response will show a toast to the user
+              // Fluttertoast.showToast(
+              // msg:title +" "+ "click",
+              // toastLength: Toast.LENGTH_SHORT,
+              // gravity: ToastGravity.CENTER,
+              // fontSize: 16.0,
 
-                  if(title == "BASICS")
-               {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => basic_page_ui()),);
-               }
-                  //Ontap response will show a toast to the user
-                 // Fluttertoast.showToast(
-                   // msg:title +" "+ "click",
-                   // toastLength: Toast.LENGTH_SHORT,
-                   // gravity: ToastGravity.CENTER,
-                   // fontSize: 16.0, 
-                    
-                    
-                    //);
-                },
-              );
-
-            }).toList(),//Here the map with all events succesfull returned card widgets
-          
-          
-          ),
-        );
-  
+              //);
+            },
+          );
+        }).toList(), //Here the map with all events succesfull returned card widgets
+      ),
+    );
   }
 }
 
 Column getCardByTitle(String title) {
-  String img=""; //Image path
-  if(title == "BASICS")
-  
-  img="assets/BASICS.png";
-  else if(title == "DSA")
-  img="assets/DSA.png";
-  else if(title == "ARRAYS")
-  img="assets/ARRAYS.png";
-  else if(title == "STRINGS")
-  img="assets/STRINGS.png";
-  else if(title == "POINTERS")
-  img="assets/POINTERS.png";
-  else if(title == "FUNTIONS")
-  img="assets/FUNCTIONS.png";
+  String img = ""; //Image path
+  if (title == "BASICS")
+    img = "assets/BASICS.png";
+  else if (title == "DSA")
+    img = "assets/DSA.png";
+  else if (title == "ARRAYS")
+    img = "assets/ARRAYS.png";
+  else if (title == "STRINGS")
+    img = "assets/STRINGS.png";
+  else if (title == "POINTERS")
+    img = "assets/POINTERS.png";
+  else if (title == "FUNTIONS") img = "assets/FUNCTIONS.png";
 
   return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        new Center(
-          child: Container(
-            child: new Stack(children: [
-                new Image.asset(img,width: 80.0,height: 80.0,)
-            ],),
-
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      new Center(
+        child: Container(
+          child: new Stack(
+            children: [
+              new Image.asset(
+                img,
+                width: 80.0,
+                height: 80.0,
+              )
+            ],
           ),
-        ),Text(title,style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold,),textAlign: TextAlign.center,)
-      ],
+        ),
+      ),
+      Text(
+        title,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      )
+    ],
   );
- 
 }
-
